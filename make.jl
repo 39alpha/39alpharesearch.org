7b22cc2a98f7deda1a3f635619c9b94dc87bbdf1
Douglas G. Moore <doug@dglmoore.com>
2019-11-11 00:40:31 -0700

    :hammer: Override baseurl

--- make.jl
+++ make.jl
@@ -1,5 +1,28 @@
 using Documenter
 
+baseurl() = "/" * get(ENV, "TRAVIS_TAG", "latest")
+
+function with_baseurl(func, baseurl=baseurl())
+    jekyll_config = joinpath(@__DIR__, "_config.yml")
+    lines = readlines(jekyll_config, keep=true)
+    open(jekyll_config, "w+") do f
+        for line in lines
+            if startswith(line, "baseurl:")
+                write(f, "baseurl: \"$(baseurl)\"\n")
+                continue
+            end
+            write(f, line)
+        end
+    end
+    ret = func()
+    open(jekyll_config, "w+") do f
+        for line in lines
+            write(f, line)
+        end
+    end
+    return ret
+end
+
 jekyll() = run(`bundle exec jekyll build`)
 
 function deploy()
@@ -15,5 +38,5 @@ function deploy()
     )
 end
 
-jekyll()
+with_baseurl(jekyll)
 deploy()
