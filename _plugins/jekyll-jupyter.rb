087d8550600035e32657469751d25b21d3addb2c
Douglas G. Moore <doug@dglmoore.com>
2019-11-11 02:19:04 -0700

    :hammer: Extract embedded resources from notebooks

--- _plugins/jekyll-jupyter.rb
+++ _plugins/jekyll-jupyter.rb
@@ -7,6 +7,10 @@ module Jekyll
             notebooks.each do |notebook|
                 dir = File.join('notebooks', notebook.basename)
                 site.pages << NotebookPage.new(site, site.source, dir, notebook)
+                resources = %x[ python3 _plugins/jupyter-extract.py #{notebook.path} ]
+                resources.split(/\r?\n/).each do |resource|
+                    site.static_files << TemporaryFile.new(site, site.source, dir, resource)
+                end
             end
             site.static_files = site.static_files - notebooks
         end
@@ -23,8 +27,16 @@ module Jekyll
             self.read_yaml(File.join(base, '_layouts'), 'page.html')
             self.data['layout'] = 'page'
             self.data['title'] = notebook.basename
-            self.data['permalink'] = "/notebooks/#{notebook.basename}"
-            self.content = %x[ python3 _plugins/jupyterconvert.py #{notebook.path} ]
+            self.data['permalink'] = "/notebooks/#{notebook.basename}/"
+            self.content = %x[ python3 _plugins/jupyter-convert.py #{notebook.path} ]
+        end
+    end
+
+    class TemporaryFile < StaticFile
+        def initialize(site, base, dir, filename)
+            super(site, base, dir, File.basename(filename))
+
+            ObjectSpace.define_finalizer(self, proc { File.delete(filename) })
         end
     end
 end
