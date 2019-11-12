646a41deb50de35b37f844e820f378290f003bcd
Douglas G. Moore <doug@dglmoore.com>
2019-11-11 19:20:51 -0700

    :hammer: Move projects page contents to a layout template

--- _plugins/projects.rb
+++ _plugins/projects.rb
@@ -18,21 +18,10 @@ module Jekyll
 
             self.process(@name)
 
-            self.read_yaml(File.join(base, '_layouts'), 'page.html')
-            self.data['layout'] = 'page'
+            self.read_yaml(File.join(base, '_layouts'), 'project_index.html')
+            self.data['layout'] = 'project_index'
             self.data['title'] = 'Projects'
             self.data['permalink'] = '/projects/'
-            self.content = '''
-                {% for project in site.projects %}
-                    <h2><a href="{{ project.url | relative_url }}">{{ project.title }}</a></h2>
-                    <p>{{ project.excerpt }}</p>
-                    <ul>
-                        <li>Purity: {{ project.purity }}</li>
-                        <li>Risk: {{ project.risk }}</li>
-                        <li>Reward: {{ project.reward }}</li>
-                    <ul>
-                {% endfor %}
-            '''
         end
     end
 end
