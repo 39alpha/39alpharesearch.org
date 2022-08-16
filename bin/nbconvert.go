12d90043cb4f952e1c4d5f0eb4d4b1213faf34b5
Douglas G. Moore <doug@39alpharesearch.org>
2022-08-15 21:38:51 -0700

    Update nbconvert to use quarto

--- bin/nbconvert.go
+++ bin/nbconvert.go
@@ -1,278 +1,352 @@
 package main
 
 import (
-    "archive/zip"
-    "fmt"
-    "io"
-    "io/ioutil"
-    "log"
-    "os"
-    "os/exec"
-    "path/filepath"
-    "regexp"
-    "text/template"
+	"archive/zip"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"sync"
 )
 
 const (
-    checkpoints = ".ipynb_checkpoints"
-    notebook_path = "notebooks"
-    notebook_name = "notebook.ipynb"
-    draft_notebook_name = "notebook_draft.ipynb"
-    markdown_name = "index.md"
-    jupyter_convert = "bin/jupyter-convert.py"
-    extract_assets = "bin/extract-assets.py"
+	checkpoints      = ".ipynb_checkpoints"
+	notebook_path    = "notebooks"
+	notebook_name    = "notebook.ipynb"
+	quarto_name      = "notebook.qmd"
+	markdown_name    = "index.md"
+	generated_assets = "notebook_files"
 )
 
 var content_root = filepath.Join("content", "notebooks")
-var markdown_template = template.Must(template.New("Notebook").Parse(`---
-title: {{ .Title }}
-draft: {{ .Draft }}
----
-{{ .Content }}`))
 
-func PythonExe() string {
-    python_exe, err := exec.LookPath("python3")
-    if err != nil {
-        python_exe, err = exec.LookPath("python")
-        if err != nil {
-            log.Fatalf("Could not find `python` or `python3` in your PATH; perhaps you need to install it?")
-        }
-    }
-    return python_exe
+func QuartoExe() string {
+	quarto, err := exec.LookPath("quarto")
+	if err != nil {
+		log.Fatalf("Could not find `quarto`; perhaps you need to install it?")
+	}
+	return quarto
 }
 
 func CopyFile(src, dst string) error {
-    in, err := os.Open(src)
-    if err != nil {
-        return err
-    }
-    defer in.Close()
+	in, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer in.Close()
 
-    out, err := os.Create(dst)
-    if err != nil {
-        return err
-    }
-    defer out.Close()
+	out, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
 
-    _, err = io.Copy(out, in)
-    if err != nil {
-        return err
-    }
-    return out.Close()
+	_, err = io.Copy(out, in)
+	if err != nil {
+		return err
+	}
+	return out.Close()
 }
 
-func WriteZip(notebook, content_dir string) error {
-    notebook_dir := filepath.Dir(notebook)
+// I don't really like making a call out to cp for this, but I don't
+// want to spend time writing a go version right now.
+func CopyDirectory(src, dst string) error {
+	cmd := exec.Command("cp", "--recursive", src, dst)
+	return cmd.Run()
+}
 
-    zipfile := filepath.Join(content_dir, "notebook.zip")
-    outFile, err := os.Create(zipfile)
-    if err != nil {
-        return err
-    }
-    defer outFile.Close()
+func (nb *Notebook) WriteZip() error {
+	notebook_dir := nb.Directory()
+	content_dir, err := nb.ContentDir()
+	if err != nil {
+		return nil
+	}
 
-    w := zip.NewWriter(outFile)
+	zipfile := filepath.Join(content_dir, "notebook.zip")
+	outFile, err := os.Create(zipfile)
+	if err != nil {
+		return err
+	}
+	defer outFile.Close()
 
-    if err := AddFiles(w, notebook_dir, ""); err != nil {
-        return err
-    }
+	w := zip.NewWriter(outFile)
 
-    if err = w.Close(); err != nil {
-        return err
-    }
+	if err := AddFiles(w, notebook_dir, ""); err != nil {
+		return err
+	}
 
-    return nil
+	if err = w.Close(); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func AddFiles(w *zip.Writer, basePath, baseInZip string) error {
-    files, err := ioutil.ReadDir(basePath)
-    if err != nil {
-        return err
-    }
+	files, err := ioutil.ReadDir(basePath)
+	if err != nil {
+		return err
+	}
 
-    for _, file := range files {
-        if !file.IsDir() {
-            data, err := ioutil.ReadFile(filepath.Join(basePath, file.Name()))
-            if err != nil {
-                return err
-            }
+	for _, file := range files {
+		if !file.IsDir() {
+			data, err := ioutil.ReadFile(filepath.Join(basePath, file.Name()))
+			if err != nil {
+				return err
+			}
 
-            f, err := w.Create(filepath.Join(baseInZip, file.Name()))
-            if err != nil {
-                return err
-            }
+			f, err := w.Create(filepath.Join(baseInZip, file.Name()))
+			if err != nil {
+				return err
+			}
 
-            _, err = f.Write(data)
-            if err != nil {
-                return err
-            }
-        } else if file.IsDir() {
-            newBase := filepath.Join(basePath, file.Name())
-            AddFiles(w, newBase, filepath.Join(baseInZip, file.Name()))
-        }
-    }
+			_, err = f.Write(data)
+			if err != nil {
+				return err
+			}
+		} else if file.IsDir() && file.Name() != checkpoints {
+			newBase := filepath.Join(basePath, file.Name())
+			AddFiles(w, newBase, filepath.Join(baseInZip, file.Name()))
+		}
+	}
 
-    return nil
+	return nil
 }
 
-func FindNotebooks() (files []string, err error) {
-    err = filepath.Walk(notebook_path, func(path string, info os.FileInfo, err error) error {
-        if err != nil {
-            return err
-        }
+func isNotebook(path string, file os.FileInfo) bool {
+	basename := filepath.Base(path)
+	return file.Mode().IsRegular() && (basename == notebook_name || basename == quarto_name)
+}
 
-        basename := filepath.Base(path)
-        if info.Mode().IsRegular() && (basename == notebook_name || basename == draft_notebook_name) {
-            files = append(files, path)
-        }
-        return nil
-    })
-    return
+type Asset struct {
+	path string
+	info os.FileInfo
 }
 
 type Notebook struct {
-    Path string
-    Title string
-    Draft bool
-    Content string
-    Assets []string
+	path   string
+	assets []*Asset
 }
 
-func ReadNotebook(path string) (*Notebook, error) {
-    dir := filepath.Dir(path)
+func FindNotebooks() (notebooks []*Notebook, err error) {
+	err = filepath.WalkDir(notebook_path, func(path string, entry os.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
 
-    title := filepath.Base(dir)
-    draft := filepath.Base(path) == draft_notebook_name
+		if entry.IsDir() {
+			return nil
+		}
 
-    cmd := exec.Command(PythonExe(), jupyter_convert, path)
+		info, err := entry.Info()
+		if err != nil {
+			return err
+		}
 
-    content, err := cmd.Output()
-    if err != nil {
-        return nil, err
-    }
-
-    re := regexp.MustCompile(`^\s*#[^\n]+`)
-    header := re.Find(content)
-    if (header != nil ) {
-        re = regexp.MustCompile(`#`)
-        i := re.FindIndex(header)[0] + 1
-        for header[i] == ' ' {
-            i += 1
-        }
-        title = string(header[i:len(header)])
-    }
-
-    assets := []string{}
-    err = filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
-        if err != nil {
-            return err
-        }
-
-        basename := filepath.Base(path)
-        if info.Mode().IsRegular() && basename != notebook_name && basename != draft_notebook_name {
-            for dir := filepath.Dir(path); dir != "/" && dir != "." && dir != ".." && dir != ""; dir = filepath.Dir(dir) {
-                if filepath.Base(dir) == checkpoints {
-                    return nil
-                }
-            }
-            assets = append(assets, path)
-        }
-        return nil
-    })
-
-    if err != nil {
-        return nil, err
-    }
-
-    return &Notebook{ path, title, draft, string(content), assets }, nil
+		if isNotebook(path, info) {
+			nb := &Notebook{path, []*Asset{}}
+			if err = nb.FindAssets(); err != nil {
+				return err
+			}
+			notebooks = append(notebooks, nb)
+		}
+		return nil
+	})
+	return
 }
 
-func (nb *Notebook) CopyAssets(dir string) error {
-    for _, asset := range nb.Assets {
-        dest_dir, err := ContentDir(asset)
-        if err != nil {
-            return err
-        }
-        dest := filepath.Join(dest_dir, filepath.Base(asset))
-        fmt.Printf("%s → %s\n", asset, dest)
-        if err = os.MkdirAll(dest_dir, os.ModeDir|os.ModePerm); err != nil {
-            return fmt.Errorf("cannot create asset directory %q: %v", dest_dir, err)
-        }
-
-        CopyFile(asset, dest)
-    }
-    return nil
+func (nb *Notebook) FindAssets() error {
+	dir := nb.Directory()
+	files, err := os.ReadDir(dir)
+	if err != nil {
+		return err
+	}
+	for _, file := range files {
+		path := nb.AssetPath(file.Name())
+		info, err := file.Info()
+		if err != nil {
+			return err
+		}
+		if isNotebook(path, info) || file.Name() == checkpoints {
+			continue
+		}
+		nb.assets = append(nb.assets, &Asset{path, info})
+	}
+	return nil
 }
 
-func (nb *Notebook) ExtractAssets(dir string) error {
-    cmd := exec.Command(PythonExe(), extract_assets, "--notebook", nb.Path, "--outdir", dir)
+func (nb *Notebook) ContentDir() (string, error) {
+	return ContentDir(nb.path)
+}
 
-    content, err := cmd.Output()
-    if err != nil {
-        return err
-    }
-    fmt.Print(string(content))
-    return nil
+func (nb *Notebook) Render() error {
+	cwd, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+	cmd := exec.Command(QuartoExe(), "render", nb.BaseName(), "--to=hugo", "--output", markdown_name)
+	cmd.Dir = filepath.Join(cwd, nb.Directory())
+	return cmd.Run()
+}
+
+func (nb *Notebook) CopyAssets() error {
+	dest_dir, err := nb.ContentDir()
+	if err != nil {
+		return err
+	}
+	for _, asset := range nb.assets {
+		dest := filepath.Join(dest_dir, filepath.Base(asset.path))
+		if err = os.MkdirAll(dest_dir, os.ModeDir|os.ModePerm); err != nil {
+			return fmt.Errorf("cannot create asset directory %q: %v", dest_dir, err)
+		}
+
+		if asset.info.IsDir() {
+			CopyDirectory(asset.path, dest)
+		} else {
+			CopyFile(asset.path, dest)
+		}
+	}
+	return nil
+}
+
+func (nb *Notebook) Directory() string {
+	return filepath.Dir(nb.path)
+}
+
+func (nb *Notebook) GeneratedAssets() []*Asset {
+	assets := []*Asset{}
+
+	dir := nb.Directory()
+	paths := []string{
+		filepath.Join(dir, markdown_name),
+		filepath.Join(dir, generated_assets),
+	}
+
+	for _, path := range paths {
+		if info, err := os.Stat(path); err == nil {
+			assets = append(assets, &Asset{path, info})
+		}
+	}
+
+	return assets
+}
+
+func (nb *Notebook) MoveGeneratedAssets() error {
+	dest_dir, err := nb.ContentDir()
+	if err != nil {
+		return err
+	}
+	for _, asset := range nb.GeneratedAssets() {
+		dest := filepath.Join(dest_dir, filepath.Base(asset.path))
+		if err := os.Rename(asset.path, dest); err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 func ContentDir(path string) (string, error) {
-    relpath, err := filepath.Rel(notebook_path, path)
-    if err != nil {
-        return "", err
-    }
-    relpath = filepath.Dir(relpath)
-    return filepath.Join(content_root, relpath), nil
+	relpath, err := filepath.Rel(notebook_path, path)
+	if err != nil {
+		return "", err
+	}
+	relpath = filepath.Dir(relpath)
+	return filepath.Join(content_root, relpath), nil
+}
+
+func (nb *Notebook) Cleanup() error {
+	for _, asset := range nb.GeneratedAssets() {
+		if err := os.RemoveAll(asset.path); err != nil {
+			return err
+		}
+	}
+
+	if nb.IsQuarto() {
+		return os.RemoveAll(nb.AssetPath(notebook_name))
+	}
+
+	return nil
+}
+
+func (nb *Notebook) BaseName() string {
+	return filepath.Base(nb.path)
+}
+
+func (nb *Notebook) AssetPath(path string) string {
+	return filepath.Join(nb.Directory(), path)
+}
+
+func (nb *Notebook) IsQuarto() bool {
+	return nb.BaseName() == quarto_name
+}
+
+func (nb *Notebook) Execute() error {
+	content_dir, err := nb.ContentDir()
+	if err != nil {
+		return fmt.Errorf("error determining content directory for notebook %q: %v\n", nb.path, err)
+	}
+
+	if err = os.MkdirAll(content_dir, os.ModeDir|os.ModePerm); err != nil {
+		return fmt.Errorf("error creating notebook directory: %v\n", err)
+	}
+
+	fmt.Printf("%s → %s\n", nb.path, content_dir)
+
+	err = nb.CopyAssets()
+	if err != nil {
+		return fmt.Errorf("error copying assets to content directory for notebook %q: %v\n", nb.path, err)
+	}
+
+	err = nb.Render()
+	if err != nil {
+		return fmt.Errorf("error converting notebook %q: %v\n", nb.path, err)
+	}
+	defer nb.Cleanup()
+
+	err = nb.MoveGeneratedAssets()
+	if err != nil {
+		return fmt.Errorf("error copying generated assets to content directory for notebook %q: %v\n", nb.path, err)
+	}
+
+	if err = nb.WriteZip(); err != nil {
+		return fmt.Errorf("error writing notebook zip: %v\n", err)
+	}
+
+	return nil
 }
 
 func main() {
-    if err := os.RemoveAll(content_root); err != nil {
-        log.Fatalf("error removing notebooks directory %q: %v\n", content_root, err)
-    }
+	if err := os.RemoveAll(content_root); err != nil {
+		log.Fatalf("error removing notebooks directory %q: %v\n", content_root, err)
+	}
 
-    notebooks, err := FindNotebooks()
-    if err != nil {
-        log.Fatalf("error finding notebooks; %v\n", err)
-    }
+	notebooks, err := FindNotebooks()
+	if err != nil {
+		log.Fatalf("error finding notebooks; %v\n", err)
+	}
 
-    if err = os.MkdirAll(content_root, os.ModeDir|os.ModePerm); err != nil {
-        log.Fatalf("error creating notebook content directory: %v\n", err)
-    }
+	if err = os.MkdirAll(content_root, os.ModeDir|os.ModePerm); err != nil {
+		log.Fatalf("error creating notebook content directory: %v\n", err)
+	}
 
-    if _, err := os.Create(filepath.Join(content_root, "_index.md")); err != nil {
-        log.Fatalf("error creating the notebook listing page: %v\n", err)
-    }
+	if _, err := os.Create(filepath.Join(content_root, "_index.md")); err != nil {
+		log.Fatalf("error creating the notebook listing page: %v\n", err)
+	}
 
-    for _, notebook := range notebooks {
-        content_dir, err := ContentDir(notebook)
-        if err != nil {
-            log.Fatalf("error determining content directory for notebook %q: %v\n", notebook, err)
-        }
-        fmt.Printf("%s → %s\n", notebook, content_dir)
-        nb, err := ReadNotebook(notebook)
-        if err != nil {
-            log.Fatalf("error reading notebook %q: %v\n", notebook, err)
-        }
+	var wg sync.WaitGroup
 
-        if err = os.MkdirAll(content_dir, os.ModeDir|os.ModePerm); err != nil {
-            log.Fatalf("error creating notebook directory: %v\n", err)
-        }
+	for _, notebook := range notebooks {
+		wg.Add(1)
+		go func(nb *Notebook) {
+			defer wg.Done()
+			if err := nb.Execute(); err != nil {
+				fmt.Fprintf(os.Stderr, "%v\n", err)
+			}
+		}(notebook)
+	}
 
-        notebook_filename := filepath.Join(content_dir, markdown_name)
-        if file, err := os.Create(notebook_filename); err != nil {
-            log.Fatalf("error opening file %q: %v\n", notebook_filename, err)
-        } else {
-            if err = markdown_template.Execute(file, nb); err != nil {
-                log.Fatalf("error executing template: %v\n", err)
-            }
-            if err = file.Close(); err != nil {
-                log.Fatalf("error closing file %q: %v\n", notebook_filename, err)
-            }
-        }
-        nb.CopyAssets(content_dir)
-        nb.ExtractAssets(content_dir)
-
-        if err = WriteZip(notebook, content_dir); err != nil {
-            log.Fatalf("error writing notebook zip: %v\n", err)
-        }
-    }
+	wg.Wait()
 }
